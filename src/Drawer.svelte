dcb923132b190dca45eb335cbb2aea29c37ab03d
Joaquin Florius <j@florius.com.ar>
2021-02-11 20:33:54 +0100

    Split preferences out

--- src/Drawer.svelte
+++ src/Drawer.svelte
@@ -3,29 +3,7 @@
     import { _ } from "svelte-i18n";
     export let opened: boolean;
 
-    import { units } from "./lib/units";
-    import {
-        volumeFamily,
-        weightFamily,
-        temperatureFamily,
-        lengthFamily,
-    } from "./myRecipeStore";
-
-    function changeWeight(ev: any) {
-        weightFamily.set(ev.currentTarget.value);
-    }
-
-    function changeVolume(ev: any) {
-        volumeFamily.set(ev.currentTarget.value);
-    }
-
-    function changeTemperature(ev: any) {
-        temperatureFamily.set(ev.currentTarget.value);
-    }
-
-    function changeLength(ev: any) {
-        lengthFamily.set(ev.currentTarget.value);
-    }
+    import Preferences from "./Preferences.svelte";
 </script>
 
 <main style={`width: ${opened ? 250 : 0}px`}>
@@ -54,67 +32,7 @@
         </ul>
 
         <h1>{$_("nav.preferences")}</h1>
-        <div class="option">
-            {$_("recipe.weight")}<br />
-            <select on:change={changeWeight}>
-                {#each Object.keys(units.weight.families) as family}
-                    <option value={family} selected={$weightFamily == family}
-                        >{units.weight.families[family]
-                            .map(({ name }) =>
-                                $_("units." + name, { default: name })
-                            )
-                            .join(", ")}</option
-                    >
-                {/each}
-            </select>
-        </div>
-
-        <div class="option">
-            {$_("recipe.volume")}<br />
-            <select on:change={changeVolume}>
-                {#each Object.keys(units.volume.families) as family}
-                    <option value={family} selected={$volumeFamily == family}
-                        >{units.volume.families[family]
-                            .map(({ name }) =>
-                                $_("units." + name, { default: name })
-                            )
-                            .join(", ")}</option
-                    >
-                {/each}
-            </select>
-        </div>
-        <div class="option">
-            {$_("recipe.temperature")}<br />
-            <select on:change={changeTemperature}>
-                {#each Object.keys(units.temperature.families) as family}
-                    <option
-                        value={family}
-                        selected={$temperatureFamily == family}
-                        >{units.temperature.families[family]
-                            .map(({ name }) =>
-                                $_("units." + name, { default: name })
-                            )
-                            .join(", ")}</option
-                    >
-                {/each}
-            </select>
-        </div>
-        <div class="option">
-            {$_("recipe.length")}<br />
-            <select on:change={changeLength}>
-                {#each Object.keys(units.length.families) as family}
-                    <option
-                        value={family}
-                        selected={$lengthFamily == family}
-                        >{units.length.families[family]
-                            .map(({ name }) =>
-                                $_("units." + name, { default: name })
-                            )
-                            .join(", ")}</option
-                    >
-                {/each}
-            </select>
-        </div>
+        <Preferences />
     </div>
 </main>
 
@@ -122,23 +40,7 @@
     .wrap {
         margin: 0 1em;
     }
-    select {
-        cursor: pointer;
-        border-radius: 0;
-        appearance: none;
-        border: none;
-        padding: 0 1em 0 0;
-        margin: 0;
-        border-radius: 5px;
-        font-family: inherit;
-        font-size: inherit;
-        cursor: inherit;
-        line-height: inherit;
-        background-color: var(--color-background-input-sidebar);
-        border-bottom: 2px solid var(--color-primary);
-        color: var(--color-primary);
-    }
-
+    
     main {
         height: 100%;
         width: 0;
@@ -151,10 +53,5 @@
         overflow-x: hidden;
         transition: width 0.5s;
         box-shadow: 4px 0 4px rgba(0, 0, 0, 0.25);
-    }
-
-    .option {
-        white-space: pre;
-        padding: 8px;
     }
 </style>
---
+++ src/Preferences.svelte
@@ -0,0 +1,111 @@
+<script lang="ts">
+    import { _ } from "svelte-i18n";
+
+    import { units } from "./lib/units";
+    import {
+        volumeFamily,
+        weightFamily,
+        temperatureFamily,
+        lengthFamily,
+    } from "./myRecipeStore";
+
+    function changeWeight(ev: any) {
+        weightFamily.set(ev.currentTarget.value);
+    }
+
+    function changeVolume(ev: any) {
+        volumeFamily.set(ev.currentTarget.value);
+    }
+
+    function changeTemperature(ev: any) {
+        temperatureFamily.set(ev.currentTarget.value);
+    }
+
+    function changeLength(ev: any) {
+        lengthFamily.set(ev.currentTarget.value);
+    }
+</script>
+
+<main>
+    <div class="option">
+        {$_("recipe.weight")}<br />
+        <select on:change={changeWeight}>
+            {#each Object.keys(units.weight.families) as family}
+                <option value={family} selected={$weightFamily == family}
+                    >{units.weight.families[family]
+                        .map(({ name }) =>
+                            $_("units." + name, { default: name })
+                        )
+                        .join(", ")}</option
+                >
+            {/each}
+        </select>
+    </div>
+
+    <div class="option">
+        {$_("recipe.volume")}<br />
+        <select on:change={changeVolume}>
+            {#each Object.keys(units.volume.families) as family}
+                <option value={family} selected={$volumeFamily == family}
+                    >{units.volume.families[family]
+                        .map(({ name }) =>
+                            $_("units." + name, { default: name })
+                        )
+                        .join(", ")}</option
+                >
+            {/each}
+        </select>
+    </div>
+    <div class="option">
+        {$_("recipe.temperature")}<br />
+        <select on:change={changeTemperature}>
+            {#each Object.keys(units.temperature.families) as family}
+                <option value={family} selected={$temperatureFamily == family}
+                    >{units.temperature.families[family]
+                        .map(({ name }) =>
+                            $_("units." + name, { default: name })
+                        )
+                        .join(", ")}</option
+                >
+            {/each}
+        </select>
+    </div>
+    <div class="option">
+        {$_("recipe.length")}<br />
+        <select on:change={changeLength}>
+            {#each Object.keys(units.length.families) as family}
+                <option value={family} selected={$lengthFamily == family}
+                    >{units.length.families[family]
+                        .map(({ name }) =>
+                            $_("units." + name, { default: name })
+                        )
+                        .join(", ")}</option
+                >
+            {/each}
+        </select>
+    </div>
+</main>
+
+<style>
+    select {
+        cursor: pointer;
+        border-radius: 0;
+        appearance: none;
+        border: none;
+        padding: 0 1em 0 0;
+        margin: 0;
+        border-radius: 5px;
+        font-family: inherit;
+        font-size: inherit;
+        cursor: inherit;
+        line-height: inherit;
+        background-color: var(--color-background-input-sidebar);
+        border-bottom: 2px solid var(--color-primary);
+        color: var(--color-primary);
+    }
+
+    .option {
+        white-space: pre;
+        padding: 8px;
+    }
+</style>
